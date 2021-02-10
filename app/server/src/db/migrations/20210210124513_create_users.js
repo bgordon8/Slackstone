bd82dac5dc52fb11ce8dd1aba50df74151468243
bgordon8 <gordonbrian16@gmail.com>
2021-02-10 13:28:38 -0700

    adds initial users db schema

--- app/server/src/db/migrations/20210210124513_create_users.js
+++ app/server/src/db/migrations/20210210124513_create_users.js
@@ -1,3 +1,14 @@
-exports.up = function (knex) {};
+exports.up = function (knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments();
+    table.text('username').unique().notNullable;
+    table.text('email').unique().notNullable;
+    table.text('password').notNullable;
+    table.enum('role', ['ADMIN', 'USER']).defaultTo('USER');
+    table.timestamps(true, true);
+  });
+};
 
-exports.down = function (knex) {};
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('users');
+};
